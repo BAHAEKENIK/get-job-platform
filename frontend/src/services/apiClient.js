f9db442633254868b8b8f126493c9cbc38a1bf40
bahae kenikssi <bahaekenik@gmail.com>
2025-09-26 13:43:44 +0100

    feat: Configure frontend for production

--- frontend/src/services/apiClient.js
+++ frontend/src/services/apiClient.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api', // L'URL de votre API Laravel
-    withCredentials: true, // Autorise l'envoi de cookies d'authentification
+    baseURL: import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/api` : 'http://127.0.0.1:8000/api',
+    withCredentials: true,
 });
 
 export default apiClient;
